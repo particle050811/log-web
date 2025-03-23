b604d9a76fd165576d8b99d257059c48380ccedd
particle050811 <2667013482@qq.com>
2025-03-23 19:00:51 +0800

    0.0.3

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ import json
 app = Flask(__name__)
 CORS(app)
 contents=["心跳发送成功","BOT_WS链接已断开，正在尝试重连……","BOT_WS链接已因长时间未收到消息而主动断开","BOT_WS链接已断开，正在尝试重连……"]
+levels=["WARNING","ERROR"]
+
 @app.route('/logs/<month>/<day>')
 def get_logs(month, day):
     if not re.match(r'^\d{1,2}$', month) or not re.match(r'^\d{1,2}$', day):
@@ -43,11 +45,14 @@ def get_logs(month, day):
         match = log_pattern.match(log)
         if match:
             content = match.group(3) + ('\n' + '\n'.join(log.split('\n')[1:]) if len(log.split('\n')) > 1 else '')
+            level = match.group(2)
+            if level in levels:
+                continue
             if content in contents:
                 continue
             processed_logs.append({
                 "timestamp": match.group(1),
-                "level": match.group(2),
+                "level": level,
                 "content": content
             })
     
